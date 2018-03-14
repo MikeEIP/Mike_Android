3c20316e6707f87f926627d1bcfff4fdfe4ca52f
fagnol_r <romain.fagnola@epitech.eu>
2018-03-14 17:53:20 +0100

    [~] Edit UI and add Register View

--- app/src/main/java/application/mike/Login.kt
+++ app/src/main/java/application/mike/Login.kt
@@ -1,13 +1,31 @@
 package application.mike
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.widget.Button
+import android.widget.Toast
+import application.mike.R.layout.activity_register
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val LoginButton = findViewById<Button>(R.id.Login)
+
+        LoginButton.setOnClickListener{
+                Toast.makeText(this@Login, "Login", Toast.LENGTH_LONG).show()
+        }
+
+        val register = findViewById<Button>(R.id.register)
+
+        register.setOnClickListener {
+            startActivity(Intent(this@Login, Register::class.java))
+        }
+
     }
 }
---
+++ app/src/main/java/application/mike/Register.kt
@@ -0,0 +1,12 @@
+package application.mike
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Register : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
