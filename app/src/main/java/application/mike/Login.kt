9fac39f3d1256e2d1e107d6dd94a7ff700083ac7
august_t <thomas.augusto@epitech.eu>
2018-03-16 10:55:07 +0100

    [~]connect renamed and moved

--- app/src/main/java/application/mike/Login.kt
+++ app/src/main/java/application/mike/Login.kt
@@ -8,6 +8,9 @@ import android.view.animation.Animation
 import android.widget.Button
 import android.widget.Toast
 import application.mike.R.layout.activity_register
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.result.Result
+import org.json.JSONObject
 
 class Login : AppCompatActivity() {
 
@@ -20,7 +23,7 @@ class Login : AppCompatActivity() {
 
         LoginButton.setOnClickListener{
                 //Toast.makeText(this@Login, "Login", Toast.LENGTH_LONG).show()
-            MikeLogin()
+            connect()
         }
 
         val register = findViewById<Button>(R.id.register)
@@ -30,4 +33,22 @@ class Login : AppCompatActivity() {
         }
 
     }
+
+    fun connect() {
+        val rootObject= JSONObject()
+        rootObject.put("username","theSnoop")
+        rootObject.put("password","thePass")
+
+        Fuel.post("https://mike.arrogant.space/v1/login").header(Pair("Content-Type", "application/json")).body(rootObject.toString()).responseString { request, response, result ->
+            when (result) {
+                is Result.Success -> {
+                    println("Result: ${result.get()}")
+                }
+                is Result.Failure -> {
+                    println("Fail déso gros")
+                }
+            }
+        }
+    }
+
 }
