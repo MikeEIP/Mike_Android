702eb45dd38f2b66962f1d9a057bc04e78e7a1b7
fagnol_r <romain.fagnola@epitech.eu>
2018-03-16 11:19:02 +0100

    [~] Login from screen + User view

--- app/src/main/java/application/mike/Login.kt
+++ app/src/main/java/application/mike/Login.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.WindowManager
 import android.view.animation.Animation
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import application.mike.R.layout.activity_register
 import com.github.kittinunf.fuel.Fuel
@@ -13,8 +14,7 @@ import com.github.kittinunf.result.Result
 import org.json.JSONObject
 
 class Login : AppCompatActivity() {
-
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -22,8 +22,13 @@ class Login : AppCompatActivity() {
         val LoginButton = findViewById<Button>(R.id.Login)
 
         LoginButton.setOnClickListener{
-                //Toast.makeText(this@Login, "Login", Toast.LENGTH_LONG).show()
-            connect()
+            var login = findViewById<EditText>(R.id.Username)
+            var password = findViewById<EditText>(R.id.Password)
+
+
+            //Toast.makeText(this@Login, "Login", Toast.LENGTH_LONG).show()
+            connect(login.text.toString(), password.text.toString())
+
         }
 
         val register = findViewById<Button>(R.id.register)
@@ -34,17 +39,19 @@ class Login : AppCompatActivity() {
 
     }
 
-    fun connect() {
+    fun connect(name: String, password: String) {
         val rootObject= JSONObject()
-        rootObject.put("username","theSnoop")
-        rootObject.put("password","thePass")
+        rootObject.put("username", name)
+        rootObject.put("password", password)
 
         Fuel.post("https://mike.arrogant.space/v1/login").header(Pair("Content-Type", "application/json")).body(rootObject.toString()).responseString { request, response, result ->
             when (result) {
                 is Result.Success -> {
+                    startActivity(Intent(this@Login, UserProfil::class.java))
                     println("Result: ${result.get()}")
                 }
                 is Result.Failure -> {
+                    Toast.makeText(this@Login, "Error Login", Toast.LENGTH_LONG).show()
                     println("Fail déso gros")
                 }
             }
---
+++ app/src/main/java/application/mike/UserProfil.kt
@@ -0,0 +1,12 @@
+package application.mike
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserProfil : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profil)
+    }
+}
