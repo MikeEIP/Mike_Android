86533c3321f306af1ed51fe7d112072203d3b79e
Cyril Puccio <cyril.puccio@epitech.eu>
2018-03-22 18:30:43 +0100

    [+] Training get api response

--- app/src/main/java/application/mike/fragments/TrainingFragment.kt
+++ app/src/main/java/application/mike/fragments/TrainingFragment.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,10 +15,6 @@ import com.github.kittinunf.fuel.android.core.Json
 import com.github.kittinunf.fuel.android.extension.responseJson
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
-import org.json.JSONException
-import org.json.JSONObject
-
-
 
 class TrainingFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,17 +26,25 @@ class TrainingFragment : Fragment() {
         val URL : String = "https://mike.arrogant.space/v1/trainings"
         var training: Json
         val view = inflater!!.inflate(R.layout.fragment_training, container, false)
-//        var ProfilTextView: TextView? = null
-//        var LevelTextView: TextView? = null
-//        var mAdapter: ProfilFragment? = null
-//        ProfilTextView = view.findViewById(R.id.Salle) as TextView
-//        LevelTextView = view.findViewById(R.id.Level) as TextView
+        var trainingTextView: TextView? = null
+        trainingTextView = view.findViewById(R.id.exercise) as TextView
 
         URL.httpGet().header(Pair("Authorization", "Bearer ${AccessToken.ACCESS_TOKEN}")).responseJson()
         { request, response, result ->
             when (result) {
                 is Result.Success -> {
+                    training = result.get()
+                    var profileObject = training.array()
 
+                    for (i in 0 until (profileObject.length())) {
+                        val item = profileObject.getJSONObject(i)
+                        var exercise = item.getString("exercise")
+                        println(exercise)
+                        trainingTextView!!.text = exercise
+                    }
+//                    var exercise = profileObject.getString()
+//                    ProfilTextView!!.text = username
+//                    LevelTextView!!.setText("lvl" + xp)
                 }
                 is Result.Failure -> {
 //                    Context().(this@ProfilFragment, "Check your connexion", Toast.LENGTH_LONG).show()
