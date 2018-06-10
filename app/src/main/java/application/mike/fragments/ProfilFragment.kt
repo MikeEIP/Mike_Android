0e015dad9bc9975da892278204687f7a42517d78
fagnol_r <romain.fagnola@epitech.eu>
2018-06-10 11:28:19 +0200

    [~] Add import

--- app/src/main/java/application/mike/fragments/ProfilFragment.kt
+++ app/src/main/java/application/mike/fragments/ProfilFragment.kt
@@ -2,6 +2,7 @@ package application.mike.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTabHost
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
